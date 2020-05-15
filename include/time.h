9b7920b4f8f8f334b9d0c4ef305db0d2e8dfdd28
leitner <leitner>
2020-05-15 12:35:32 +0000

    add getcpu and clock_getres vdso

--- include/time.h
+++ include/time.h
@@ -10,12 +10,18 @@ __BEGIN_DECLS
 
 int __isleap(int year);
 
-#define CLOCK_REALTIME           0
-#define CLOCK_MONOTONIC          1
-#define CLOCK_PROCESS_CPUTIME_ID 2
-#define CLOCK_THREAD_CPUTIME_ID  3
-#define CLOCK_REALTIME_HR        4
-#define CLOCK_MONOTONIC_HR       5
+/* from linux/time.h */
+#define CLOCK_REALTIME			0
+#define CLOCK_MONOTONIC			1
+#define CLOCK_PROCESS_CPUTIME_ID	2
+#define CLOCK_THREAD_CPUTIME_ID		3
+#define CLOCK_MONOTONIC_RAW		4
+#define CLOCK_REALTIME_COARSE		5
+#define CLOCK_MONOTONIC_COARSE		6
+#define CLOCK_BOOTTIME			7
+#define CLOCK_REALTIME_ALARM		8
+#define CLOCK_BOOTTIME_ALARM		9
+#define CLOCK_TAI			11
 
 int clock_settime(clockid_t clock_id,const struct timespec*tp);
 int clock_gettime(clockid_t clock_id,struct timespec*tp);
