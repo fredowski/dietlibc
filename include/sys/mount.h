78ed08fd24c22b2e84009ec1d839540048441247
leitner <leitner>
2020-06-04 11:59:35 +0000

    typo

--- include/sys/mount.h
+++ include/sys/mount.h
@@ -115,7 +115,7 @@ enum
 {
   MNT_FORCE = 1,		/* Force unmounting.  */
 #define MNT_FORCE MNT_FORCE
-  MNT_DETACH = 2		/* Just detach, unmount when last reference dies.  */
+  MNT_DETACH = 2,		/* Just detach, unmount when last reference dies.  */
 #define MNT_DETACH MNT_DETACH
   MNT_EXPIRE = 4,		/* Mark for expiry.  */
 #define MNT_EXPIRE MNT_EXPIRE
