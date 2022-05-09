78ecc0f0202e7dd35dbd6b7f8ff1e272650ed492
leitner <leitner>
2022-05-09 14:07:46 +0000

    implement several updates proposed by Guilherme Janczak

--- lib/arc4random.c
+++ lib/arc4random.c
@@ -10,24 +10,41 @@ static unsigned int n;
 
 /* These come from OpenBSD: */
 uint32_t arc4random(void) {
-  if (n==0) arc4random_stir();
+  if (n==0) arc4random_buf(buf, sizeof buf);
   uint32_t r=buf[n];
   if (++n > sizeof(buf)/sizeof(buf[0])) n=0;
   return r;
 }
 
 void arc4random_buf(void* Buf, size_t N) {
+  static int enosys=0;
+  int old_errno=errno;
   int r;
-  if ((size_t)(r=getrandom(Buf,N,GRND_NONBLOCK)) != N) {
-    if (r==-1 && errno==ENOSYS) {
-      int fd=open("/dev/urandom",O_RDONLY);
+  int fd=-1;
+  while (N>0) {
+    if (!enosys) 
+      r=getrandom(Buf,N,0);
+    else {
+      // fallback code in case kernel doesn't have getrandom
+      if (fd==-1) fd=open("/dev/urandom",O_RDONLY);
       if (fd==-1) abort();
       r=read(fd,Buf,N);
-      close(fd);
     }
-    if ((size_t)r != N)
+    if (r<=0) {
+      switch(errno) {
+      case ENOSYS:
+	enosys=1;	// kernel doesn't have getrandom (pre 3.17)
+	/* fall through */
+      case EINTR:
+	continue;
+      }
       abort();
+    }
+    Buf += r;	// getrandom has a limit of 32MiB, so loop if necessary
+    N -= r;
   }
+  if (fd!=-1) close(fd);
+  errno=old_errno;	// man page doesn't mention changes to errno
 }
 
 uint32_t arc4random_uniform(uint32_t upper_bound) {
@@ -54,15 +71,30 @@ uint32_t arc4random_uniform(uint32_t upper_bound) {
   return r % upper_bound;
 }
 
-void arc4random_stir(void) {
-  n=0;
-  arc4random_buf(buf,sizeof(buf));
-}
+#ifdef UNITTEST
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
 
-void arc4random_addrandom(unsigned char* dat,size_t datlen) {
-  // not actually doing it, as all the randomness comes from the kernel
-  // anyway
-  (void)dat;
-  (void)datlen;
+int main() {
+  const size_t n = 64*1024*1024;
+  unsigned char* buf=malloc(n);	// 64 MB
+  memset(buf,0,n);
+  arc4random_buf(buf,n);
+  size_t i;
+  size_t buckets[256];
+  for (i=0; i<256; ++i) buckets[i]=0;
+  for (i=0; i<n; ++i)
+    buckets[buf[i]]++;
+  size_t l,h;
+  l=h=buckets[0];
+  // count how often each byte happened
+  for (i=1; i<256; ++i) {
+    if (buckets[i]<l) l=buckets[i];
+    if (buckets[i]>h) h=buckets[i];
+  }
+  // difference between highest and lowest count should be < 10%
+  assert(l*1000/h > 90);
 }
-
+#endif
