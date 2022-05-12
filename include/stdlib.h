24e1eac82354d15a077db2f7972f767e601c79f7
leitner <leitner>
2022-05-12 13:40:13 +0000

    remove arc4random_stir and arc4random_addrandom (both have been removed from OpenBSD as well)

--- include/stdlib.h
+++ include/stdlib.h
@@ -205,9 +205,6 @@ void arc4random_buf(void* buf, size_t n) __THROW;
 __attribute__((__warn_unused_result__))
 uint32_t arc4random_uniform(uint32_t upper_bound) __THROW;
 
-void arc4random_stir(void) __THROW;
-void arc4random_addrandom(unsigned char* dat,size_t datlen) __THROW;
-
 __END_DECLS
 
 #endif
