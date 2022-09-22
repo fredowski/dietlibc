386f17813cb449d5e9d4b3cba28ef1cb01a43447
leitner <leitner>
2022-09-22 07:14:02 +0000

    fix wcsncmp (behaved like memcmp not strcmp) (Gabriel Ravier)

--- lib/wcsncmp.c
+++ lib/wcsncmp.c
@@ -2,6 +2,6 @@
 
 int wcsncmp(const wchar_t *s1, const wchar_t *s2, size_t n) {
   size_t i;
-  for (i=0; i<n && s1[i]==s2[i]; ++i) ;
+  for (i=0; i<n && s1[i] && s1[i]==s2[i]; ++i) ;
   return s1[i]-s2[i];
 }
