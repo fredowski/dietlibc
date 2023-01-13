65d9139460de0e7e1ee41260fa4a357fc2796032
leitner <leitner>
2023-01-13 17:09:58 +0000

    test for case where someone memchrs for 257 (arg is int but only lower 8 bits should be compared)

--- test/memchr.c
+++ test/memchr.c
@@ -10,5 +10,8 @@ int main() {
   assert(memchr(test,'b',7)==test);
   assert(memchr(test+6,'r',1)==test+6);
   assert(memchr(test+7,0,100)==test+7);
+
+  const char* test2="\x01\x02\x00\x00\x00";
+  assert(memchr(test2, 258, 5) == test2+1);
   return 0;
 }
