284dc9126bbd3e3c597388ab5dceb9b0d7b3fbdd
leitner <leitner>
2024-04-05 16:03:44 +0000

    add new futex flags

--- include/sys/futex.h
+++ include/sys/futex.h
@@ -12,6 +12,15 @@ enum {
   FUTEX_FD=2,
   FUTEX_REQUEUE=3,
   FUTEX_CMP_REQUEUE=4,
+  FUTEX_WAKE_OP=5,
+  FUTEX_LOCK_PI=6,
+  FUTEX_UNLOCK_PI=7,
+  FUTEX_TRYLOCK_PI=8,
+  FUTEX_WAIT_BITSET=9,
+  FUTEX_WAKE_BITSET=10,
+  FUTEX_WAIT_REQUEUE_PI=11,
+  FUTEX_CMP_REQUEUE_PI=12,
+  FUTEX_LOCK_PI2=13,
 };
 
 long futex(int* uaddr,int op,int val,const struct timespec* timeout,int* uaddr2,int val3);
