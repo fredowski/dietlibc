cae8bae1e12623ff90b0fc4eb7ba97d5a4c92473
leitner <leitner>
2024-04-05 17:35:24 +0000

    fix futex calls to use the right reference clock

--- lib/cnd_timedwait.c
+++ lib/cnd_timedwait.c
@@ -5,7 +5,7 @@
 int cnd_timedwait(cnd_t* cond, mtx_t* mutex, const struct timespec* time_point) {
   int r;
   do {
-    r=futex(&cond->sem,FUTEX_WAIT,0,time_point,0,0);
+    r=futex(&cond->sem,FUTEX_WAIT_BITSET_PRIVATE|FUTEX_CLOCK_REALTIME,0,time_point,0,FUTEX_BITSET_MATCH_ANY);
     if (r==-1) {
       if (errno==EWOULDBLOCK) break;
       else if (errno==EINTR) continue;
