6f715c2a937350f1ef11582cbefa1b4b8715e201
leitner <leitner>
2024-10-02 16:52:52 +0000

    add qsort_r (_GNU_SOURCE only, needed by zstd)

--- include/stdlib.h
+++ include/stdlib.h
@@ -109,6 +109,10 @@ double erand48(randbuf buf) __THROW;
 
 __attribute__((__callback(4, 1, 1)))
 void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
+#ifdef _GNU_SOURCE
+__attribute__((__callback(4, 1, 1)))
+void qsort_r(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *, void *), void* cookie);
+#endif
 
 __attribute__((__callback(5, 1, 2)))
 __attribute__((__warn_unused_result__))
--- lib/qsort.c
+++ lib/qsort.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/cdefs.h>
 #include <stdlib.h>
 #include "rand_i.h"
@@ -19,7 +20,8 @@ static void exch(char* base,size_t size,size_t a,size_t b) {
 /* Blame him for the scary variable names */
 /* http://www.cs.princeton.edu/~rs/talks/QuicksortIsOptimal.pdf */
 static void quicksort(char* base,size_t size,ssize_t l,ssize_t r,
-		      int (*compar)(const void*,const void*)) {
+		      int (*compar)(const void*,const void*,void*),
+		      void* cookie) {
   ssize_t i=l-1, j=r, p=l-1, q=r, k;
   char* v=base+r*size;
   if (r<=l) return;
@@ -43,31 +45,31 @@ static void quicksort(char* base,size_t size,ssize_t l,ssize_t r,
 #endif
 
   for (;;) {
-    while (++i != r && compar(base+i*size,v)<0) ;
-    while (compar(v,base+(--j)*size)<0) if (j == l) break;
+    while (++i != r && compar(base+i*size,v,cookie)<0) ;
+    while (compar(v,base+(--j)*size,cookie)<0) if (j == l) break;
     if (i >= j) break;
     exch(base,size,i,j);
-    if (compar(base+i*size,v)==0) exch(base,size,++p,i);
-    if (compar(v,base+j*size)==0) exch(base,size,j,--q);
+    if (compar(base+i*size,v,cookie)==0) exch(base,size,++p,i);
+    if (compar(v,base+j*size,cookie)==0) exch(base,size,j,--q);
   }
   exch(base,size,i,r); j = i-1; ++i;
   for (k=l; k<p; k++, j--) exch(base,size,k,j);
   for (k=r-1; k>q; k--, i++) exch(base,size,i,k);
-  quicksort(base,size,l,j,compar);
-  quicksort(base,size,i,r,compar);
+  quicksort(base,size,l,j,compar,cookie);
+  quicksort(base,size,i,r,compar,cookie);
+}
+
+void qsort_r(void* base,size_t nmemb,size_t size,int (*compar)(const void*,const void*,void*),void* cookie) {
+  /* check for integer overflows */
+  size_t prod;
+  if (nmemb >= (((size_t)-1)>>1) ||
+      size >= (((size_t)-1)>>1) ||
+      __builtin_mul_overflow(nmemb,size,&prod)) return;
+  if (nmemb>1)
+    quicksort(base,size,0,nmemb-1,compar,cookie);
 }
 
 void qsort(void* base,size_t nmemb,size_t size,int (*compar)(const void*,const void*)) {
-  /* check for integer overflows */
-  if (nmemb >= (((size_t)-1)>>1) ||
-      size >= (((size_t)-1)>>1)) return;
-#if 0
-  if (sizeof(size_t) < sizeof(unsigned long long)) {
-    if ((unsigned long long)size * nmemb > (size_t)-1) return;
-  } else {
-    if (size*nmemb/nmemb != size) return;
-  }
-#endif
-  if (nmemb>1)
-    quicksort(base,size,0,nmemb-1,compar);
+  qsort_r(base,nmemb,size,(void*)compar,0);
 }
+
