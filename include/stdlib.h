85cff85b2d29e5acbdede0622dbdbac1f3389048
leitner <leitner>
2024-11-12 11:12:01 +0000

    fix callback attribute for qsort_r

--- include/stdlib.h
+++ include/stdlib.h
@@ -110,7 +110,7 @@ double erand48(randbuf buf) __THROW;
 __attribute__((__callback(4, 1, 1)))
 void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
 #ifdef _GNU_SOURCE
-__attribute__((__callback(4, 1, 1)))
+__attribute__((__callback(4, 1, 1, 5)))
 void qsort_r(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *, void *), void* cookie);
 #endif
 
