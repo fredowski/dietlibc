dcb493a6f7a266049881cf10c0a7238e368285e3
leitner <leitner>
2024-11-12 13:17:31 +0000

    fix typos in stdckdint.h

--- include/stdckdint.h
+++ include/stdckdint.h
@@ -6,8 +6,8 @@
 /* *result = a+b; return true on arithmetic overflow */
 #define ckd_add(result, a, b) __builtin_add_overflow(a,b,result)
 /* *result = a-b; return true on arithmetic overflow */
-#define ckh_sub(result, a, b) __builtin_sub_overflow(a,b,result)
+#define ckd_sub(result, a, b) __builtin_sub_overflow(a,b,result)
 /* *result = a*b; return true on arithmetic overflow */
-#define ckh_mul(result, a, b) __builtin_mul_overflow(a,b,result)
+#define ckd_mul(result, a, b) __builtin_mul_overflow(a,b,result)
 
 #endif
