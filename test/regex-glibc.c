4e0b86154971ca9975236b29cdb0d9c908ce2c80
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2024-11-28 18:02:36 +0100

    test: new regex-glibc regular expression test introduced
    This test is copied from glibc and tests various regular
    expressions. I extended the test to also check that the match
    result offsets are in a correct range and that the match results
    are correct.

--- test/regex-glibc.c
+++ test/regex-glibc.c
@@ -1,5 +1,7 @@
 /***********************************************************
 
+Copied from glibc, adapted - 2024 - Friedrich Beckmann
+
 Copyright 1995 by Tom Lord
 
                         All Rights Reserved
@@ -22,31 +24,37 @@ PERFORMANCE OF THIS SOFTWARE.
 
 ******************************************************************/
 
-
-
 #include <sys/types.h>
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-
+#define IGNORE_OUTOFRANGE 1
 
 struct a_test
 {
-  int expected;
+  int expected; /* 0: Compiles, finds matches, 1: No matches found, 2: Compile error*/
   const char * pattern;
   const char * data;
+  const char *matches[10];
 };
 
 static const struct a_test the_tests[] =
 {
-#include "testcases.h"
+#include "regex-glibc-testcases.h"
+  {0, "ab((c*)d)ef", "abcdefg", { "abcdef", "cd", "c", NULL } },
   {-1, 0, 0}
 };
 
-
-
+int
+regmatch_out_of_range (regmatch_t reg, size_t len)
+{
+  return (reg.rm_so < 0 || reg.rm_so > len ||
+          reg.rm_eo < 0 || reg.rm_eo > len ||
+          reg.rm_eo < reg.rm_so);
+}
 
 static int
 run_a_test (int id, const struct a_test * t)
@@ -57,6 +65,8 @@ run_a_test (int id, const struct a_test * t)
   char errmsg[100];
   int x;
   regmatch_t regs[10];
+  int match_error = false;
+  size_t len = strlen(t->data);
 
   if (!last_pattern || strcmp (last_pattern, t->pattern))
     {
@@ -89,25 +99,73 @@ run_a_test (int id, const struct a_test * t)
 
   err = regexec (&r, t->data, 10, regs, 0);
 
-  if (err != t->expected)
+  if (err == 0) { // dut thinks we have a match
+    int no_of_matches = 0;
+    for (x = 0;x < 10; ++x) {
+      if (regs[x].rm_so == -1) {
+        if (t->matches[x] == (const char *)NULL)
+          continue;
+        else {
+          match_error = true;
+          break;
+        }
+      } else {
+#if IGNORE_OUTOFRANGE
+        if ( t->matches[x] == (const char *)NULL )
+          continue;
+#endif
+        if ( (! regmatch_out_of_range(regs[x], len)) &&
+             ( t->matches[x] != (const char *)NULL ) &&
+             strncmp(t->data + (size_t) regs[x].rm_so,
+                     t->matches[x],
+                     (size_t) (regs[x].rm_eo - regs[x].rm_so)) == 0 ) {
+          no_of_matches++;
+          continue;
+        } else {
+          match_error = true;
+          break;
+        }
+      }
+    }
+    if (no_of_matches == 0)
+      match_error = true;
+  }
+
+  if ((err == 0 && t->expected != 0) ||
+      (err != 0 && t->expected == 0) ||
+      match_error)
     {
-      printf ("test %d\n", id);
       printf ("pattern \"%s\" data \"%s\" wanted %d got %d\n",
 	      t->pattern, t->data, t->expected, err);
-      for (x = 0; x < 10; ++x)
-	printf ("reg %d == (%d, %d) %.*s\n",
+      if (err == 0) {
+        size_t len = strlen(t->data);
+        for (x = 0; x < 10; ++x) {
+          if (regs[x].rm_so == -1)
+            printf("reg %d == (-1, U) act: no match - exp: %s\n",
+                x,
+                (t->matches[x] == (const char *)NULL ? "no match" : t->matches[x]));
+          else if (regmatch_out_of_range(regs[x], len)) {
+            printf("reg %d == (%ld, %ld) exp: %s\n",
+              x,
+              regs[x].rm_so,
+              regs[x].rm_eo,
+              (t->matches[x] == (const char *)NULL ? "no match" : t->matches[x]));
+          } else {
+            printf ("reg %d == (%ld, %ld) act: %.*s exp: %s\n",
 		x,
 		regs[x].rm_so,
 		regs[x].rm_eo,
-		regs[x].rm_eo - regs[x].rm_so,
-		t->data + regs[x].rm_so);
+		(int) (regs[x].rm_eo - regs[x].rm_so),
+		t->data + (size_t) regs[x].rm_so,
+                (t->matches[x] == (const char *)NULL ? "no match": t->matches[x]));
+          }
+        }
+      }
       return 1;
     }
   puts (" OK.");
   return 0;
 }
-
-
 
 int
 main (int argc, char * argv[])
